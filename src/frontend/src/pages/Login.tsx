4407e30099c816c4c0c36c21255f85852441369a
Miachi Solomon <smiachi10@gmail.com>
2026-03-26 22:17:16 +0100

    Register and Login form validations

--- src/frontend/src/pages/Login.tsx
+++ src/frontend/src/pages/Login.tsx
@@ -1,20 +1,52 @@
 import { Link } from "react-router-dom";
 import { BackButton } from "../components/BackButton";
 import styles from "../static/Login.module.css";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <section>
       <BackButton />
       <div id={styles.loginContainer}>
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <h1>Login</h1>
-          <input type="email" placeholder="Enter your email" name="email" />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            {...register("email", { required: "Email is required" })}
+          />
+          {errors.email && (
+            <span className={styles.errorMsg}>{errors.email.message}</span>
+          )}
           <input
             type="password"
             placeholder="Enter your password"
-            name="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Password must be 8 characters long",
+              },
+            })}
           />
+          {errors.password && (
+            <span className={styles.errorMsg}>{errors.password.message}</span>
+          )}
           <button type="submit" id={styles.loginBtn}>
             Login
           </button>
--- src/frontend/src/pages/Signup.tsx
+++ src/frontend/src/pages/Signup.tsx
@@ -1,74 +1,87 @@
-import { useState, type ChangeEvent } from "react";
 import styles from "../static/Signup.module.css";
 import { Link } from "react-router-dom";
 import { BackButton } from "../components/BackButton";
+import { useForm, type SubmitHandler } from "react-hook-form";
 
-interface UserInfo {
+type Inputs = {
   email: string;
   username: string;
   password: string;
-}
-
-// Add form validation to signup and login
-
-const initState: UserInfo = { email: "", username: "", password: "" };
+  confirmPassword: string;
+};
 
 export function Signup() {
-  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
 
-  const [userInfo, setUserInfo] = useState<UserInfo>(initState);
-
-  const handleEmail = (e: ChangeEvent<HTMLInputElement>): void => {
-    setUserInfo({ ...userInfo, email: e.target.value });
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    // const response = await fetch("http:localhost:3000/api/users", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // });
+    console.log(data);
   };
 
-  const handleUsername = (e: ChangeEvent<HTMLInputElement>): void => {
-    setUserInfo({ ...userInfo, username: e.target.value });
-  };
-
-  const handlePassword = (e: ChangeEvent<HTMLInputElement>): void => {
-    setUserInfo({ ...userInfo, password: e.target.value });
-  };
-
-  const handleConfirmPassword = (e: ChangeEvent<HTMLInputElement>): void => {
-    setConfirmPassword(e.target.value);
-  };
+  const password = watch("password");
 
   return (
     <section>
       <BackButton />
       <div id={styles.formContainer}>
-        {/* post to an api */}
-        <form action="" id={styles.signUpForm}>
+        <form id={styles.signUpForm} onSubmit={handleSubmit(onSubmit)}>
           <h1>Register</h1>
           <input
             type="email"
+            {...register("email", { required: "Email is required" })}
             placeholder="Enter your email"
-            value={userInfo.email}
-            onChange={handleEmail}
-            required
           />
+          {errors.email && (
+            <span className={styles.errorMsg}>{errors.email.message}</span>
+          )}
           <input
             type="text"
+            {...register("username", { required: "Username is required" })}
             placeholder="Enter a username"
-            value={userInfo.username}
-            onChange={handleUsername}
-            required
           />
+          {errors.username && (
+            <span className={styles.errorMsg}>{errors.username.message}</span>
+          )}
           <input
             type="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: "Password must be 8 characters long",
+              },
+            })}
             placeholder="Create your password"
-            value={userInfo.password}
-            onChange={handlePassword}
-            required
           />
+          {errors.password && (
+            <span className={styles.errorMsg}>{errors.password.message}</span>
+          )}
           <input
             type="password"
+            {...register("confirmPassword", {
+              required: "Confirm password correctly",
+              validate: (value) => {
+                return value === password || "Passwords do not match";
+              },
+            })}
             placeholder="Confirm your password"
-            value={confirmPassword}
-            onChange={handleConfirmPassword}
-            required
           />
+          {errors.confirmPassword && (
+            <span className={styles.errorMsg}>
+              {errors.confirmPassword.message}
+            </span>
+          )}
           <button type="submit" id={styles.signUpBtn}>
             Register
           </button>
