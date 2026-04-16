48c35860567893073a905767cce49709d98a9e95
Miachi Solomon <smiachi10@gmail.com>
2026-04-16 15:46:54 +0100

    Editing files and configurations for deployment of backend servers

--- src/backend/README.md
+++ src/backend/README.md
@@ -1,17 +1,5 @@
 # BACKEND FOR DM'S
 
-### Rest API
+### APIs
 
-The Rest API runs on port 3000 and exposes some endpoints that are needed for the frontend to run properly and fetch certain data.
-
-Rest APIs were built using expressjs and typescript
-
-[REST APIs](https://github.com/miachis/dms/tree/main/src/backend/src/api/rest)
-
-### Websocket API
-
-WebSocket API is on port 4000 and enables bi-directional communication between two users at the same time, Websockets were used to prevent latency and allow messages to be sent in real time
-
-Websocket APIs were built using socket.io and typescript
-
-[WEBSOCKET API](https://github.com/miachis/dms/blob/main/src/backend/src/webSocketServer.ts)
+Both REST and Websockets run on the same port and source code for both can be found at [source](https://github.com/miachis/dms/blob/main/src/backend/src/app.ts)
--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,5 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
+import http from "http";
+import { Server } from "socket.io";
+import { prisma } from "../lib/prisma.js";
 import express from "express";
 import cors from "cors";
 import router from "./api/rest/router.js";
@@ -9,6 +12,10 @@ import passportJwt from "../config/passportJwt.js";
 passportJwt(passport);
 import { upload } from "./api/rest/supabase/upload.js";
 import { uploads } from "../config/multer.js";
+
+// END OF IMPORTS
+
+/* EXPRESS SERVER AND ITS LOGIC */
 
 const app = express();
 app.use(express.json());
@@ -21,6 +28,99 @@ app.post("/upload/:id", uploads.single("profilePicture"), upload);
 app.use("/api/users", router.users);
 app.use("/messages", router.messages);
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+// END OF EXPRESS SERVER
+
+/* WEBSOCKET SERVER */
+
+const server = http.createServer(app); /*HTTP SERVER*/
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://dms-ivory.vercel.app",
+  },
+});
+
+/* WEBSOCKET LOGIC */
+
+io.on("connection", (socket) => {
+  socket.emit("id", socket.id);
+
+  const fullMaps = new Map();
+
+  socket.on("send_id", (id: string) => {
+    fullMaps.set(id, socket.id);
+  });
+
+  socket.on("join room", async (room: string, userId: string) => {
+    socket.join(room);
+
+    let conversation = await prisma.conversations.findFirst({
+      where: {
+        name: room,
+      },
+      include: {
+        participants: true,
+      },
+    });
+
+    if (!conversation) {
+      // Create conversation + first participant
+      conversation = await prisma.conversations.create({
+        data: {
+          name: room,
+          participants: {
+            create: [
+              {
+                userId: Number(userId),
+              },
+            ],
+          },
+        },
+        include: {
+          participants: true,
+        },
+      });
+    } else {
+      // Check if user already in participants
+      const alreadyParticipant = conversation.participants.find(
+        (p) => p.userId === Number(userId),
+      );
+
+      if (!alreadyParticipant) {
+        await prisma.participants.create({
+          data: {
+            userId: Number(userId),
+            conversationId: conversation.id,
+          },
+        });
+      }
+    }
+  });
+
+  socket.on(
+    "send message",
+    async (message: string, room: string, senderId: string) => {
+      const conversation = await prisma.conversations.findFirst({
+        where: {
+          name: room,
+        },
+      });
+
+      if (!conversation) return;
+
+      const newMessage = await prisma.messages.create({
+        data: {
+          message: message,
+          senderId: Number(senderId),
+          conversationId: conversation.id,
+        },
+      });
+      io.to(room).emit("message sent", message, senderId);
+    },
+  );
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
